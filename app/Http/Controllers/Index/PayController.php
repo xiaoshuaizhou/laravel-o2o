78c04463b9be0f253582d23cf29526ab8869d58f
zhou <792831332@qq.com>
2017-06-29 18:33:38 +0800

    搭建支付页面

--- app/Http/Controllers/Index/PayController.php
+++ app/Http/Controllers/Index/PayController.php
@@ -7,7 +7,15 @@ use App\Http\Controllers\Controller;
 
 class PayController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth');
+    }
     public function index($id) {
-        return '订单成功！！！';
+        $title = '支付页';
+        $controller = '';
+        $city = session('city');
+        $citys = session('citys');
+        $cats = session('cats');
+        return view('index.pay.index', compact('title', 'controller', 'city', 'citys', 'cats'));
     }
 }
